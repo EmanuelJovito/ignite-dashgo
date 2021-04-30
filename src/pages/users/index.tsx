c742fce681bf26c6e48dc17129d70f0d6d7c4fc0
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-04-30 09:06:18 -0300

    feat: Criando responsividade da UserList

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,10 +1,15 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true
+  })
+
   return (
     <Box>
       <Header />
@@ -30,17 +35,17 @@ export default function UserList() {
           <Table colorScheme='whiteAlpha'>
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" width="8">
+                <Th px={["4", "4", "6"]} color="gray.300" width="8">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
-                <Th width="8"></Th>
+                { isWideVersion && <Th>Data de cadastro</Th> }
+                { isWideVersion ? ( <Th width="8"></Th> ) : ""}
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -49,10 +54,9 @@ export default function UserList() {
                     <Text fontSize="sm" color="gray.300">emauel_carneiro@hotmail.com</Text>
                   </Box>
                 </Td>
-                <Td>
-                  29 de Abril, 2021
-                </Td>
-                <Td>
+                { isWideVersion && <Td>29 de Abril, 2021</Td> }
+                { isWideVersion ? (
+                  <Td>
                   <Button 
                     as="a" 
                     size="sm" 
@@ -63,9 +67,10 @@ export default function UserList() {
                     Editar
                   </Button>
                 </Td>
+                ) : ''}
               </Tr>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -74,10 +79,9 @@ export default function UserList() {
                     <Text fontSize="sm" color="gray.300">emauel_carneiro@hotmail.com</Text>
                   </Box>
                 </Td>
-                <Td>
-                  29 de Abril, 2021
-                </Td>
-                <Td>
+                { isWideVersion && <Td>29 de Abril, 2021</Td> }
+                { isWideVersion ? (
+                  <Td>
                   <Button 
                     as="a" 
                     size="sm" 
@@ -88,9 +92,10 @@ export default function UserList() {
                     Editar
                   </Button>
                 </Td>
+                ) : ''}
               </Tr>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -99,10 +104,9 @@ export default function UserList() {
                     <Text fontSize="sm" color="gray.300">emauel_carneiro@hotmail.com</Text>
                   </Box>
                 </Td>
-                <Td>
-                  29 de Abril, 2021
-                </Td>
-                <Td>
+                { isWideVersion && <Td>29 de Abril, 2021</Td> }
+                { isWideVersion ? (
+                  <Td>
                   <Button 
                     as="a" 
                     size="sm" 
@@ -113,6 +117,7 @@ export default function UserList() {
                     Editar
                   </Button>
                 </Td>
+                ) : ''}
               </Tr>
             </Tbody>
           </Table>
