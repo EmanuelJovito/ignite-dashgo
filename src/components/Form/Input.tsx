d4cfdbe357d475ca0fb80592c01ad54bcf343e1c
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-04-30 11:12:08 -0300

    feat: Criando validação de dados no formulario de SignIn

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,15 +1,17 @@
 import { forwardRef, ForwardRefRenderFunction } from "react";
-import { FormControl, FormLabel, Input as ChackraInput, InputProps as ChackraInputProps } from "@chakra-ui/react";
+import { FormControl, FormErrorMessage, FormLabel, Input as ChackraInput, InputProps as ChackraInputProps } from "@chakra-ui/react";
+import { FieldError } from "react-hook-form";
 
 interface InputProps extends ChackraInputProps {
   name: string,
   label?: string
+  error?: FieldError
 }
 
 const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> 
-  = ({ name, label, ...rest }, ref) => {
+  = ({ name, label, error = null, ...rest }, ref) => {
     return (
-      <FormControl>
+      <FormControl isInvalid={!!error}>
         <FormLabel htmlFor={name}>{label}</FormLabel>
 
         <ChackraInput
@@ -25,6 +27,12 @@ const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps>
           ref={ref}
           {...rest}
         />
+
+        { !!error && (
+          <FormErrorMessage>
+            {error.message}
+          </FormErrorMessage>
+        )}
       </FormControl>
     )
 }
