0840b68a7bfafe7f3e4ab4867e2ccba8905ae4ad
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-04-30 10:37:48 -0300

    feat: Criando form de autenticação

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,3 +1,4 @@
+import { forwardRef, ForwardRefRenderFunction } from "react";
 import { FormControl, FormLabel, Input as ChackraInput, InputProps as ChackraInputProps } from "@chakra-ui/react";
 
 interface InputProps extends ChackraInputProps {
@@ -5,23 +6,27 @@ interface InputProps extends ChackraInputProps {
   label?: string
 }
 
-export function Input({ name, label, ...rest }: InputProps) {
-  return (
-    <FormControl>
-      <FormLabel htmlFor={name}>{label}</FormLabel>
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> 
+  = ({ name, label, ...rest }, ref) => {
+    return (
+      <FormControl>
+        <FormLabel htmlFor={name}>{label}</FormLabel>
 
-      <ChackraInput
-        name={name} 
-        id={name}
-        focusBorderColor="pink.500"
-        bgColor="gray.900"
-        variant="filled"
-        _hover={{
-          bgColor: "gray.900"
-        }}
-        size="lg"
-        {...rest}
-      />
-    </FormControl>
-  )
+        <ChackraInput
+          name={name} 
+          id={name}
+          focusBorderColor="pink.500"
+          bgColor="gray.900"
+          variant="filled"
+          _hover={{
+            bgColor: "gray.900"
+          }}
+          size="lg"
+          ref={ref}
+          {...rest}
+        />
+      </FormControl>
+    )
 }
+
+export const Input = forwardRef(InputBase)
