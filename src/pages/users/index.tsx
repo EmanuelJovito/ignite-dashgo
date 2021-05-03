d2a04f05287a09edade3dafda4524445a6a8ffc9
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-05-03 10:50:05 -0300

    feat: Criando listagem de usuários vindos da api

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,7 +12,20 @@ export default function UserList() {
     const response = await fetch('http://localhost:3000/api/users')
     const data = await response.json()
 
-    return data;
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      }
+    });
+    
+    return users
   })
 
   const isWideVersion = useBreakpointValue({
@@ -68,81 +81,22 @@ export default function UserList() {
                 </Tr>
               </Thead>
               <Tbody>
-                <Tr>
-                  <Td px={["4", "4", "6"]}>
-                    <Checkbox colorScheme="pink" />
-                  </Td>
-                  <Td>
-                    <Box>
-                      <Text fontWeight="bold">Emanuel Jovito</Text>
-                      <Text fontSize="sm" color="gray.300">emauel_carneiro@hotmail.com</Text>
-                    </Box>
-                  </Td>
-                  { isWideVersion && <Td>29 de Abril, 2021</Td> }
-                  { isWideVersion ? (
-                    <Td>
-                    <Button 
-                      as="a" 
-                      size="sm" 
-                      fontSize="sm" 
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                    >
-                      Editar
-                    </Button>
-                  </Td>
-                  ) : ''}
-                </Tr>
-                <Tr>
-                  <Td px={["4", "4", "6"]}>
-                    <Checkbox colorScheme="pink" />
-                  </Td>
-                  <Td>
-                    <Box>
-                      <Text fontWeight="bold">Emanuel Jovito</Text>
-                      <Text fontSize="sm" color="gray.300">emauel_carneiro@hotmail.com</Text>
-                    </Box>
-                  </Td>
-                  { isWideVersion && <Td>29 de Abril, 2021</Td> }
-                  { isWideVersion ? (
-                    <Td>
-                    <Button 
-                      as="a" 
-                      size="sm" 
-                      fontSize="sm" 
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                    >
-                      Editar
-                    </Button>
-                  </Td>
-                  ) : ''}
-                </Tr>
-                <Tr>
-                  <Td px={["4", "4", "6"]}>
-                    <Checkbox colorScheme="pink" />
-                  </Td>
-                  <Td>
-                    <Box>
-                      <Text fontWeight="bold">Emanuel Jovito</Text>
-                      <Text fontSize="sm" color="gray.300">emauel_carneiro@hotmail.com</Text>
-                    </Box>
-                  </Td>
-                  { isWideVersion && <Td>29 de Abril, 2021</Td> }
-                  { isWideVersion ? (
-                    <Td>
-                    <Button 
-                      as="a" 
-                      size="sm" 
-                      fontSize="sm" 
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                    >
-                      Editar
-                    </Button>
-                  </Td>
-                  ) : ''}
-                </Tr>
+                {data.map(user => {
+                  return (
+                    <Tr key={user.id}>
+                      <Td px={["4", "4", "6"]}>
+                        <Checkbox colorScheme="pink" />
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight="bold">{user.name}</Text>
+                          <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                        </Box>
+                      </Td>
+                      { isWideVersion && <Td>{user.createdAt}</Td> }
+                    </Tr>
+                  )
+                })}
               </Tbody>
             </Table>
 
