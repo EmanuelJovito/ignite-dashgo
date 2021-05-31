c4a04b2e0b09d0e67e16b7af2fb86bb5c699ca38
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-05-31 08:57:05 -0300

    feat: Adicionando perfil de usuario

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,22 +1,29 @@
-import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+import {useSession} from 'next-auth/client'
+import { BiError } from 'react-icons/bi'
+import { Avatar, Box, Flex, Icon, Text } from "@chakra-ui/react";
 
 interface ProfileProps {
   showProfileData?: boolean 
 }
 
 export function Profile({ showProfileData = true }: ProfileProps) {
+  const [ session ] = useSession()
+
   return (
     <Flex align="center">
-      { showProfileData && (
+      {session && (
+        <>
         <Box mr="4" textAlign="right">
-          <Text>Emanuel Jovito</Text>
+          <Text>{session.user.name}</Text>
           <Text color="gray.300" fontSize="small">
-            emanuel_carneiro@hotmail.com
+            {session.user.email}
           </Text>
-        </Box>
-      )}
 
-      <Avatar size="md" name="Emanuel Jovito" src="https://github.com/EmanuelJovito.png"/>
+        </Box>
+
+        <Avatar size="md" name={session.user.name} src={session.user.image}/>
+        </>
+      )}
     </Flex>
   )
 }
